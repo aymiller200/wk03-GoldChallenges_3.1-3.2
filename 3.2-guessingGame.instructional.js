04826c659abc671330ccb2c99255f6f2907e27b9
aymiller200 <aymiller200@gmail.com>
2021-03-18 10:09:45 -0400

    what I have so far. Second challenge still has some bugs in it that I am trying to figure out.

--- 3.2-guessingGame.instructional.js
+++ 3.2-guessingGame.instructional.js
@@ -19,3 +19,48 @@
 //? not required:  If you liked to style it, feel free to add a stylesheet to your HTML.
 
 //NOTE: Write your code below and push back to your github branch.  SUBMIT YOUR GITHUB URL IN CANVAS
+
+
+let tries = 3
+let guess = 0
+let min = 1
+let max = 10
+let answer = Math.floor(Math.random() * (max-min + 1)) + min
+console.log(answer)
+
+
+guess = window.prompt("I'm thinking of a number between 1 and 10! Bet you can't guess it!")
+
+
+    
+    
+        parseInt(guess)
+
+        for(i = 1; i <= answer; i++){
+         
+            
+            if (guess == answer){
+                 alert('You got it!')
+    
+                break;
+            } else if (guess < answer) {
+                tries--
+                guess = prompt(`To low. ${tries} remaining`)
+                
+            } else if (guess > answer){
+                tries--
+                guess = prompt(`Too high. ${tries} remaining`)
+                
+            } 
+            
+            
+            if (tries == 0){
+                alert('Sorry, no more guesses')
+                break;
+            }
+
+
+            
+        }
+    
+
